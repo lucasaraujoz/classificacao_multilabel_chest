57479247999f6c04cf401975931f7bf97e580e9e
lucas <lcsaraujo88@gmail.com>
2024-02-04 14:43:53 -0300

    correcao formula steps

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+#VARS
 BATCH_SIZE=16
 
 import pandas as pd
@@ -9,23 +10,27 @@ from glob import glob
 from sklearn.model_selection import GroupShuffleSplit
 from sklearn.metrics import roc_auc_score
 import tensorflow as tf
-# import tensorflow.python.keras.backend as K
 import utils
 import uuid
 from keras.layers import Dense, Flatten, Dropout, BatchNormalization, Input, Concatenate
 from tensorflow.keras.layers import GlobalAveragePooling2D, Reshape, Dense, Permute, multiply
 import tensorflow.keras.backend as K
 from keras import layers
+from math import ceil
+print("Número de GPUs disponíveis: ", len(
+tf.config.list_physical_devices('GPU')))
+gpus = tf.config.experimental.list_physical_devices('GPU')
+print(gpus)
+if gpus:
+    try:
+        tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+        tf.config.experimental.set_memory_growth(gpus[0], True)
+    except RuntimeError as e:
+        print(e)
 
 
 def split_dataset():
     df = pd.read_csv('/home/lucas/dataset_chest/df_ori_mask_crop.csv')
-    # df = df.loc[:,['Image Index','Patient ID', 'Finding Labels']]
-    # img_paths={os.path.basename(x): x for x in glob(os.path.join('.', '/home/lucas_araujo/pibic-2024/dataset', 'images*','images','*.png'))} 
-    # df['path']=df['Image Index'].map(img_paths.get) #mapping image ids to all image paths
-    # labels = df['Finding Labels'].str.get_dummies('|')
-    # df = pd.concat([df, labels], axis=1)
-    #split train/test 80/20
     split = GroupShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
     groups = df['Patient ID'].values
 
@@ -42,6 +47,30 @@ def split_dataset():
     df_train_atualizado = df_train.iloc[train_idxs]
     df_val = df_train.iloc[val_idxs]
     return df_train_atualizado, df_test, df_val
+
+def get_generator(df, x_col, batch_size=BATCH_SIZE, shuffle=False):
+    datagen = ImageDataGenerator(
+        samplewise_center=True,
+        samplewise_std_normalization= True)
+
+    generator = datagen.flow_from_dataframe(
+        dataframe=df,
+        directory = None,
+        x_col=x_col,
+        y_col= labels,
+        class_mode= "raw",
+        target_size=(224,224),
+        batch_size=BATCH_SIZE,
+        shuffle=shuffle,
+    )
+    return generator
+
+def generator_two_img(gen1, gen2):
+    while True:
+        X1i = gen1.next()
+        X2i = gen2.next()
+
+        yield [X1i[0], X2i[0]], X1i[1]
 
 
 df_train, df_test, df_val = split_dataset()
@@ -63,44 +92,15 @@ labels = [
     "Pneumonia",
     "Pneumothorax"] 
 
-datagen = ImageDataGenerator(
-        samplewise_center=True,
-        samplewise_std_normalization= True)
+#generators global
+train_generator_global = get_generator(df = df_train, x_col="path", shuffle=True)
+val_generator_global = get_generator(df = df_val, x_col="path", shuffle=False)
+test_generator_global = get_generator(df = df_test, x_col="path", shuffle=False)
 
-train_generator = datagen.flow_from_dataframe(
-    dataframe=df_train,
-    directory = None,
-    x_col='path',
-    y_col= labels,
-    class_mode= "raw",
-    target_size=(224,224),
-    batch_size=BATCH_SIZE,
-    shuffle=True,
-)
-
-val_generator = datagen.flow_from_dataframe(
-    dataframe=df_val,
-    directory = None,
-    x_col='path',
-    y_col= labels,
-    class_mode= "raw",
-    target_size=(224,224),
-    batch_size=1,
-    shuffle=False,
-)
-
-
-test_generator = datagen.flow_from_dataframe(
-    dataframe=df_test,
-    directory = None,
-    x_col='path',
-    y_col= labels,
-    class_mode= "raw",
-    target_size=(224,224),
-    batch_size=1,
-    shuffle=False,
-)
-
+#generators local 
+train_generator_local = get_generator(df = df_train, x_col="path_crop", shuffle=True)
+val_generator_local = get_generator(df = df_val, x_col="path_crop", shuffle=False)
+test_generator_local = get_generator(df = df_test, x_col="path_crop", shuffle=False)
 
 
 def compute_class_freqs(labels):
@@ -125,7 +125,7 @@ def compute_class_freqs(labels):
     return positive_frequencies, negative_frequencies
 
 
-freq_pos, freq_neg = compute_class_freqs(train_generator.labels)
+freq_pos, freq_neg = compute_class_freqs(train_generator_global.labels) #usando o generator global pra calcular a frequencia 
 pos_weights = freq_neg
 neg_weights = freq_pos
 pos_contribution = freq_pos * pos_weights
@@ -175,7 +175,6 @@ def local_branch(input_shape):
     densenet121._name= 'densenet121_local_branch'
     return densenet121
 
-
 def squeeze_excite_block(tensor, ratio=16):
     init = tensor
     channel_axis = 1 if K.image_data_format() == "channels_first" else -1
@@ -192,7 +191,6 @@ def squeeze_excite_block(tensor, ratio=16):
 
     x = multiply([init, se])
     return x
-
 
 def model_fusion(local_encoder, global_encoder):
   for layer in local_encoder.layers:
@@ -219,7 +217,7 @@ def train():
     model_name = f"{uuid.uuid4()}"
     CHECKPOINT_PATH = f"{MODEL_PATH}/{model_name}"
     os.makedirs(CHECKPOINT_PATH, exist_ok=True)
-    
+    print(f"Modelo - {model_name}")
     checkpoint = tf.keras.callbacks.ModelCheckpoint(f"{CHECKPOINT_PATH}/weights.ckpt",
         monitor = 'val_loss',
         save_weights_only = True,
@@ -231,13 +229,13 @@ def train():
     early_stopping = tf.keras.callbacks.EarlyStopping(
         monitor='val_loss',
         mode='min',
-        patience=10,
+        patience=8,
         restore_best_weights=True
     )
 
 
     img_shape= (224,224,3)
-    #classificador global
+    # #classificador global
     global_encoder = global_branch(img_shape)
     x = global_encoder.output
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
@@ -245,55 +243,64 @@ def train():
     model_global = tf.keras.models.Model(inputs=global_encoder.input, outputs=predictions_global)
     model_global.compile(optimizer='adam', loss=get_weighted_loss(pos_weights, neg_weights),  metrics=[tf.keras.metrics.AUC(multi_label=True)])
 
-    H_G = model_global.fit(train_generator, 
-        validation_data = val_generator,
-        epochs = 15,
-        callbacks=[checkpoint,
-                early_stopping]
+    H_G = model_global.fit(train_generator_global, 
+        validation_data = val_generator_global,
+        epochs = 1,
+        callbacks=[
+            # checkpoint,
+            early_stopping
+            ],
         )
    
-    #classificador local
-    # local_encoder = local_branch(img_shape)
-    # x = local_encoder.output
-    # x = tf.keras.layers.GlobalAveragePooling2D()(x)
-    # predictions_local = tf.keras.layers.Dense(len(labels), activation="sigmoid")(x)
-    # model_local = tf.keras.models.Model(inputs=local_encoder.input, outputs=predictions_local)
+    # #classificador local
+    local_encoder = local_branch(img_shape)
+    x = local_encoder.output
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    predictions_local = tf.keras.layers.Dense(len(labels), activation="sigmoid")(x)
+    model_local = tf.keras.models.Model(inputs=local_encoder.input, outputs=predictions_local)
 
-    # model_local.compile(optimizer='adam', loss=get_weighted_loss(pos_weights, neg_weights),  metrics=[tf.keras.metrics.AUC(multi_label=True)])
+    model_local.compile(optimizer='adam', loss=get_weighted_loss(pos_weights, neg_weights),  metrics=[tf.keras.metrics.AUC(multi_label=True)])
 
-    # H_L = model_local.fit(train_generator, 
-    #     validation_data = val_generator,
-    #     epochs = 1,
-    #     callbacks=[checkpoint,
-    #             early_stopping]
-    #     )
+    H_L = model_local.fit(train_generator_local, 
+        validation_data = val_generator_local,
+        epochs = 1,
+        callbacks=[
+                # checkpoint,
+                early_stopping],
+        )
     
 
     # classificador fusão
-
-    # f_model = model_fusion(local_encoder, global_encoder)
-    # f_model.compile(optimizer='adam', loss=get_weighted_loss(pos_weights, neg_weights),  metrics=[tf.keras.metrics.AUC(multi_label=True)])
-    # H_F = f_model.fit(train_generator, 
-    # validation_data = val_generator,
-    # epochs = 1,
-    # callbacks=[checkpoint,
-    #         early_stopping]
-    # )
+    train_generator_global_2 = get_generator(df = df_train, x_col="path", shuffle=False) # recriando os generator com shuffe=False pra manter consistência do batch
+    train_generator_local_2 = get_generator(df = df_train, x_col="path_crop", shuffle=False)
+    
+    train_two = generator_two_img(train_generator_global_2, train_generator_local_2)
+    val_two = generator_two_img(val_generator_global, val_generator_local)
+    test_two = generator_two_img(test_generator_global, test_generator_local)
 
 
+    f_model = model_fusion(local_encoder, global_encoder)
+    f_model.compile(optimizer='adam', loss=get_weighted_loss(pos_weights, neg_weights),  metrics=[tf.keras.metrics.AUC(multi_label=True)])
+    
+    H_F = f_model.fit(train_two, 
+        validation_data = val_two,
+        epochs = 1,
+        steps_per_epoch =  ceil(len(train_generator_global_2.labels)/BATCH_SIZE),
+        validation_steps = ceil(len(val_generator_global.labels)/BATCH_SIZE),
+        callbacks=[
+            checkpoint,
+            early_stopping]
+    )
 
-
-    utils.save_history(H_G.history, CHECKPOINT_PATH)
-
-
-    predictions = model_global.predict(test_generator, verbose=1)
-    auc_scores = roc_auc_score(test_generator.labels, predictions, average=None)
-    auc_score_macro = roc_auc_score(test_generator.labels, predictions, average='macro')
-    auc_score_micro = roc_auc_score(test_generator.labels, predictions, average='micro')
-    auc_score_weighted = roc_auc_score(test_generator.labels, predictions, average='weighted')
+    utils.save_history(H_F.history, CHECKPOINT_PATH)
+    predictions = f_model.predict(test_two, verbose=1, steps= ceil(len(test_generator_global.labels)/BATCH_SIZE)) ## usando o generator como tamanho de steps
+    auc_scores = roc_auc_score(test_generator_global.labels, predictions, average=None)
+    auc_score_macro = roc_auc_score(test_generator_global.labels, predictions, average='macro')
+    auc_score_micro = roc_auc_score(test_generator_global.labels, predictions, average='micro')
+    auc_score_weighted = roc_auc_score(test_generator_global.labels, predictions, average='weighted')
     
     results = {
-        "groun_truth" : test_generator.labels,
+        "groun_truth" : test_generator_global.labels,
         "predictions" : predictions,
         "auc_scores" : auc_scores,
         "labels" : labels,
@@ -301,7 +308,7 @@ def train():
         "auc_micro" : auc_score_micro,
         "auc_weighted" : auc_score_weighted,
     }
-    utils.store_test_metrics(results, path=CHECKPOINT_PATH) 
+    utils.store_test_metrics(results, path=CHECKPOINT_PATH, filename=f"test_metrics_auc_{auc_score_macro}")
 
 if __name__ == "__main__":
      model = train()
